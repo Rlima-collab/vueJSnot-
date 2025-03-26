888351336c77dae070db16bcc22b0f7dc1c766a9
Fbouam <fahed.bouamrani@etu.univ-orleans.com>
2025-03-26 18:23:27 +0100

    essaie de la methode de mounted

---
+++ vuejsnote/src/App.vue
@@ -0,0 +1,34 @@
+<script>
+let data = {
+  questionnaires: [
+    { id : 1, name : 'Questionnaire 1' },
+    { id : 2, name : 'Questionnaire 2' },
+    { id : 3, name : 'Questionnaire 3' },
+    { id : 4, name : 'Questionnaire 4' },
+    { id : 5, name : 'Questionnaire 5' }
+  ],
+  title: 'Mes Questionnaires',
+  newItem: ''
+};
+
+export default {
+  methods: {
+    addQuestionnaires: function () {
+    },
+    mounted(){
+      fetch('http://localhost:5000/Quiz/v1.0/questionnaires')
+        .then(response => response.json())
+        .then(json => {
+          console.log("mounted");
+          console.log(json).questionnaires;
+          this.questionnaires = json.questionnaires;
+        })
+    }
+  },
+  components: { QuestionnairesItemItem}
+};
+</script>
+
+<template>
+    {{ questionnaires[1].name }}
+</template>
